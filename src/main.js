edbe8fa04ac560c146e3da3c544695e7e3cb6674
miller <441997573@qq.com>
2021-09-22 11:37:56 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import fastClick from 'fastclick'
+import VueLazyLoad from 'vue-lazyload'
+
+Vue.config.productionTip = false
+    //创建事件总线
+Vue.prototype.$bus = new Vue()
+
+fastClick.attach(document.body)
+
+Vue.use(VueLazyLoad)
+
+new Vue({
+    render: h => h(App),
+    router,
+    store,
+}).$mount('#app')
